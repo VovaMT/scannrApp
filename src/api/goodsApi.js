b5d4e8608ae30212fa3f69047ac1e40f3e2a6403
VovaMT <111053876+VovaMT@users.noreply.github.com>
2025-04-24 14:00:15 +0300

    Add license check

--- src/api/goodsApi.js
+++ src/api/goodsApi.js
@@ -1,11 +1,17 @@
 const API_URL = "http://10.0.25.51:8080/goods";
 
-export const fetchGoods = async () => {
-  const response = await fetch(API_URL);
+export const fetchGoods = async (deviceKey) => {
+  const response = await fetch(API_URL, {
+    method: "GET",
+    headers: {
+      "Device-Key": deviceKey,
+    },
+  });
+
   if (!response.ok) {
-    throw new Error("Помилка при отриманні даних з сервера");
+    const errorObj = await response.json();
+    throw new Error(errorObj.error || "Помилка при отриманні даних з сервера");
   }
 
-  const goods = await response.json();
-  return goods;
+  return await response.json();
 };
--- src/screens/DownloadScreen.js
+++ src/screens/DownloadScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, Button, Alert } from "react-native";
 import { initDB, clearGoods, insertGoods } from "../database/db";
 import { fetchGoods } from "../api/goodsApi";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const DownloadScreen = () => {
   const [loading, setLoading] = useState(false);
@@ -11,23 +12,25 @@ const DownloadScreen = () => {
     try {
       setLoading(true);
       setProgress(0.1);
-
+  
+      const deviceKey = await AsyncStorage.getItem("device_key");
+  
       await initDB();
       setProgress(0.3);
-
-      const goods = await fetchGoods();
+  
+      const goods = await fetchGoods(deviceKey); 
       setProgress(0.6);
-
+  
       await clearGoods();
       setProgress(0.8);
-
+  
       await insertGoods(goods);
       setProgress(1);
-
+  
       Alert.alert("Дані успішно збережено у SQLite!");
     } catch (error) {
-      console.error("Помилка завантаження:", error);
-      Alert.alert("Помилка", error.message || "Не вдалося завантажити дані");
+      
+      Alert.alert("Помилка", error.message || "Невідома помилка");
     } finally {
       setProgress(0);
       setLoading(false);
